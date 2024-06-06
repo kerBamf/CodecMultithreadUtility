f6b853acfff6d0acf1208fd0db6dcb24cc6edf92
kerBamf <zach.pedigo@gmail.com>
2024-06-06 12:03:19 -0400

    Adjusted multithreader so you can dynamically select which function you want to run on each codec at runtime

--- codec_multithreader.py
+++ codec_multithreader.py
@@ -1,19 +1,25 @@
 import os
 import time
 from dotenv import load_dotenv
+import concurrent.futures
 import requests
+from importlib import import_module
 from urllib3.exceptions import InsecureRequestWarning
 from openpyxl import load_workbook
-import concurrent.futures
 import smtplib
 import xml.etree.ElementTree as ET
 from excel_parser import excel_parser
-from chain_update import chain_update
+from function_selector import function_selector
 from logger import log_info
 
+#Loading environment variables
 LOGPATH = os.environ.get('LOGPATH')
 
 ip_list = excel_parser()
+selected_function = function_selector()
+imported_func = getattr(import_module(selected_function), selected_function)
+
+#function = function_selector()
 
 def message(string):
     print(string)
@@ -21,7 +27,7 @@ def message(string):
 
 def update_iterator():
     with concurrent.futures.ThreadPoolExecutor() as executor:
-        futures = {executor.submit(chain_update, ip): ip for ip in ip_list}
+        futures = {executor.submit(imported_func, ip): ip for ip in ip_list}
     
     for future in concurrent.futures.as_completed(futures):
         if (future.exception()):
---
+++ function_selector.py
@@ -0,0 +1,42 @@
+from os import listdir, getcwd
+from os.path import isfile, join
+
+#Loading find relative path
+path = getcwd()
+
+def find_functions():
+    file_list = [file for file in listdir(path) if isfile(join(path, file)) and file.endswith('.py') and file != 'codec_multithreader.py' and file != 'excel_parser.py' and file != 'logger.py' and file != 'function_selector.py']
+    list_dict = {}
+    option_string = ''
+    for idx, file in enumerate(file_list):
+        list_dict.update({idx: file})
+        option_string = option_string + f'\t{file}: {idx}\r\n'
+
+    output = {
+        'files': list_dict,
+        'options': option_string
+    }
+
+    return output
+
+found_functions = find_functions()
+
+def user_choice():
+    choice = int(input(f'Select which function to run:\r\n{found_functions['options']}\r\nFunction number selection: '))
+    for key in list(found_functions['files'].keys()):
+        if choice == key:
+            if 'y' == input(f'\n\rYou have selected to run {found_functions['files'][key]} Proceed? (y/n): '):
+                return choice
+            else:
+                print('Selection Cancelled')
+                return user_choice()
+    print('Your selection is invalid.')
+    return user_choice()
+
+def function_selector():
+    selected_function = found_functions['files'][int(user_choice())]
+    selected_function = selected_function.replace('.py', '')
+    return selected_function
+
+if __name__ == '__main__':
+    function_selector()
