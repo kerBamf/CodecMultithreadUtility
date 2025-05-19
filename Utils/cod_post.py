e52b5d2e35b86d65af374845fe3db2e2ef201f4e
kerBamf <zach.pedigo@gmail.com>
2025-05-19 14:53:42 -0400

    Adjusted cod_post to better reflect error handling, created check_clickshare to report on ClickShare presence on endpoints. Includes cross-referencing with Lightware presence to determine potential lightware issues

--- Utils/cod_post.py
+++ Utils/cod_post.py
@@ -2,52 +2,88 @@ from os import environ
 from dotenv import load_dotenv
 import requests
 
-#Loading environment varaibles
+# Loading environment varaibles
 load_dotenv()
 
-PASSCODE = environ.get('PASSCODE')
-default_headers = {
-    'Authorization': f'basic {PASSCODE}',
-    'Content-Type': 'text/xml'
-}
+PASSCODE = environ.get("PASSCODE")
+default_headers = {"Authorization": f"basic {PASSCODE}", "Content-Type": "text/xml"}
 
-#Removing insecure http warnings
+# Removing insecure http warnings
 requests.packages.urllib3.disable_warnings()
 
-#Pre-defining request in order to reduce clutter in other files. Returns text XML unless there's an error.
+
+# Pre-defining request in order to reduce clutter in other files. Returns text XML unless there's an error.
 def cod_post(ip, payload, cookie=None):
     if cookie:
-        headers = {'Cookie': f'SessionId={cookie}'}
+        headers = {"Cookie": f"SessionId={cookie}"}
     else:
         headers = default_headers
     try:
-        response = requests.post(f'http://{ip}/putxml', headers=headers, data=payload, verify=False, timeout=(10, 30))
+        response = requests.post(
+            f"http://{ip}/putxml",
+            headers=headers,
+            data=payload,
+            verify=False,
+            timeout=(10, 30),
+        )
         return response.text
+    except requests.HTTPError as err:
+        print("HTTP Connection Error")
+        return "HTTP Connection Error"
+    except requests.Timeout as err:
+        print("Connection Timeout Error")
+        return "Connection Timeout Error"
     except requests.RequestException as err:
-        print(err.response)
-        return err.response
+        print(err)
+        return err
 
-#Function for opening a codec session, decreasing latency for multiple-command scripts
+
+# Function for opening a codec session, decreasing latency for multiple-command scripts
 def cod_session_start(ip):
     try:
-        response = requests.post(f'http://{ip}/xmlapi/session/begin', headers=default_headers, verify=False, timeout=(10, 30))
-        print(response.cookies['SessionId'])
-        return response.cookies['SessionId']
+        response = requests.post(
+            f"http://{ip}/xmlapi/session/begin",
+            headers=default_headers,
+            verify=False,
+            timeout=(10, 30),
+        )
+        print(response.cookies["SessionId"])
+        return response.cookies["SessionId"]
+    except requests.HTTPError as err:
+        print("HTTP Connection Error")
+        return "HTTP Connection Error"
+    except requests.Timeout as err:
+        print("Connection Timeout Error")
+        return "Connection Timeout Error"
     except requests.RequestException as err:
-        print(err.response)
-        return err.response
-#Needed to end a codec session
-def cod_session_end(ip, cookie):
-    headers = {'Cookie': f'SessionId={cookie}'}
-    try:
-        response = requests.post(f'http://{ip}/xmlapi/session/end', headers=headers, verify=False, timeout=(10, 30))
-        return response.status_code
-    except requests.RequestException as err:
-        print(err.response)
-        return err.response
+        print(err)
+        return err
 
-if __name__ == '__main__':
-    with open('../xml_files/test_post.xml', encoding="utf-8") as f:
+
+# Needed to end a codec session
+def cod_session_end(ip, cookie):
+    headers = {"Cookie": f"SessionId={cookie}"}
+    try:
+        response = requests.post(
+            f"http://{ip}/xmlapi/session/end",
+            headers=headers,
+            verify=False,
+            timeout=(10, 30),
+        )
+        return response.status_code
+    except requests.HTTPError as err:
+        print("HTTP Connection Error")
+        return "HTTP Connection Error"
+    except requests.Timeout as err:
+        print("Connection Timeout Error")
+        return "Connection Timeout Error"
+    except requests.RequestException as err:
+        print(err)
+        return err
+
+
+if __name__ == "__main__":
+    with open("../xml_files/test_post.xml", encoding="utf-8") as f:
         test_xml = f.read()
-    result = cod_post('172.16.131.192', test_xml)
+    result = cod_post("172.16.131.192", test_xml)
     print(result)
---
+++ check_clickShare.py
@@ -0,0 +1,144 @@
+from os import environ
+from dotenv import load_dotenv
+import xml.etree.ElementTree as ET
+from time import sleep
+from Utils.logger import log_info
+from Utils.xml_selector import xml_selector
+from Utils.cod_get import cod_get
+from Utils.cod_post import cod_post
+from Utils.excel_parser import excel_parser
+import xml.etree.ElementTree as ET
+import openpyxl
+import concurrent.futures
+
+
+class CustomException(Exception):
+    pass
+
+
+# Loading environment variable
+load_dotenv()
+PASSCODE = environ.get("PASSCODE")
+LOGPATH = environ.get("LOGPATH")
+headers = {"Authorization": f"basic {PASSCODE}"}
+
+
+# Setting up logger
+def message(string, ip, path=LOGPATH):
+    log_info(string, ip, path)
+    print(string)
+
+
+def check_hdmi(codec):
+    try:
+        response = cod_get(codec.ip, "Configuration/Video/Input/Connector/Name")
+        response = response.lower().replace(" ", "").replace("-", "")
+        if response.find("clickshare") != -1:
+            return True
+        elif response.find("error") != -1:
+            message(f"Unable to retrieve info from {codec.name}", codec.name)
+            return "error"
+        else:
+            return False
+    except Exception as error:
+        message(f"Unable to retrieve info at {codec.name} --> {error}", codec.name)
+        return "error"
+
+
+external_XML = f"""<Command>
+    <UserInterface>
+        <Presentation>
+            <ExternalSource>
+                <List></List>
+            </ExternalSource>
+        </Presentation>
+    </UserInterface>
+</Command>"""
+
+
+def check_external(codec):
+    try:
+        response = cod_post(codec.ip, external_XML)
+        response = response.lower().replace(" ", "").replace("-", "")
+        if response.find("clickshare") != -1:
+            return True
+        elif response.find("error") != -1:
+            message(f"Unable to retrieve info from {codec.name}", codec.name)
+            return "error"
+        else:
+            return False
+    except Exception as error:
+        message(f"Unable to retrieve info at {codec.name} --> {error}", codec.name)
+        return "error"
+
+
+def check_lightware(codec):
+    try:
+        response = cod_get(codec.ip, "Configuration/Video/Input/Connector/Name")
+        response = response.lower().replace(" ", "").replace("-", "")
+        if response.find("source") != -1:
+            return True
+        elif response.find("error") != -1:
+            message(f"Unable to retrieve info from {codec.name}", codec.name)
+            return "error"
+        else:
+            return False
+    except Exception as error:
+        message(f"Unable to retrieve info at {codec.name} --> {error}", codec.name)
+        return "error"
+
+
+def check_inputs(codec):
+    vid_inputs = check_hdmi(codec)
+    ext_inputs = check_external(codec)
+    lightware = check_lightware(codec)
+
+    if vid_inputs == True or ext_inputs == True:
+        codec.ClickShare = "Present"
+    elif vid_inputs == "error" or ext_inputs == "error":
+        codec.ClickShare = "Unable to retrieve info"
+    else:
+        codec.ClickShare = "Not Detected"
+    if lightware == True:
+        codec.Lightware = "Present"
+    elif lightware == "error":
+        codec.Lightware = "Unable to retrive info"
+    else:
+        codec.Lightware = "Not Present"
+
+    return codec
+
+
+def check_clickShare():
+    # Loading excel workbook
+    import_list = excel_parser()
+    codecs_processed = [["Name", "IP", "ClickShare", "Lightware"]]
+
+    # Feeding through multithreader to obtain data from codecs
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        futures = {executor.submit(check_inputs, codec): codec for codec in import_list}
+
+    for future in concurrent.futures.as_completed(futures):
+        codec = future.result()
+        codecs_processed.append(
+            [codec.name, codec.ip, codec.ClickShare, codec.Lightware]
+        )
+
+    # Creating new Excel file
+    new_wb = openpyxl.Workbook()
+    new_ws = new_wb.active
+    new_ws.title = "ClickShare Status"
+    ft = openpyxl.styles.Font(bold=True)
+
+    for row in codecs_processed:
+        new_ws.append(row)
+
+    for row in new_ws["A1:C1"]:
+        for cell in row:
+            cell.font = ft
+
+    new_wb.save("../output_files/ClickShareSpreadsheet.xlsx")
+
+
+if __name__ == "__main__":
+    check_clickShare()
