37c4771e4ae3a1e9b12d179830120fe32af3b654
kerBamf <zach.pedigo@gmail.com>
2025-08-11 11:24:41 -0400

    Fixed email_utility, adding test case when called standalone and created an environment variable for output file path

--- Utils/email_utility.py
+++ Utils/email_utility.py
@@ -1,31 +1,45 @@
 import smtplib, ssl
 import mimetypes
+import os
 from email.message import EmailMessage
 from email.policy import Policy
+from dotenv import load_dotenv
 
 
-def email_utility(subject, content, send_to, send_from, file_path=None, file_name=None):
+# Importing environment variables
+load_dotenv()
+
+TEST_PATH = os.environ.get("OUTPUT_FILES_PATH")
+
+
+def email_utility(email_from, email_to, email_subject, email_text, file_path=None):
+
     msg = EmailMessage()
-    msg["Subject"] = subject
-    msg.set_content(content)
-    msg["To"] = send_to
-    msg["From"] = send_from
+    msg["From"] = email_from
+    msg["To"] = email_to
+    msg["Subject"] = email_subject
+    msg.set_content(email_text)
+
+    server = "exchange2007.mskcc.org"
+    port = 25
     if file_path != None:
-        with open(file_path, "rb") as fp:
-            msg.add_attachment(
-                fp.read(),
-                maintype="application",
-                subype="xlsx",
-                filename=file_name,
-            )
-    with smtplib.SMTP("exchange2007.mskcc.org", 25) as s:
-        s.send_message(msg)
+        file_data = open(file_path, "rb").read()
+        msg.add_attachment(
+            file_data,
+            maintype="application",
+            subtype="xlsx",
+            filename=(file_path.split("/").pop()),
+        )
+    smtp = smtplib.SMTP(server, port)
+    smtp.send_message(msg)
+    smtp.quit()
 
 
 if __name__ == "__main__":
     email_utility(
-        "Test Email",
-        "Hopefully this ish works alright",
-        "pedigoz@mkscc.com",
-        "reboot_report_donotreply@mskcc.org",
+        "reboot_report_noreply@mskcc.org",
+        "pedigoz@mskcc.org",
+        "Yup, another test email",
+        "Hey Zach,\r\n\r\nHere's the results of this week's automated reboot. Do good, you.\r\n\r\nZach the Robot",
+        TEST_PATH + "automated_reboot-08-07.xlsx",
     )
